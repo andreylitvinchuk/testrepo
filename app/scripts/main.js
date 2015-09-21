99dc29c7e890054da39d156ab2f05c5e72796666
Nikita Solomennikov <nikitosgroover@gmail.com>
2015-09-21 11:47:08 +0300

    init commit

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+// Tabs swither
+$('.tabs').on('click', '.tabs_buttons li', function(event) {
+	event.preventDefault();
+	/* Act on the event */
+	var index = $(this).index();
+	$('.tabs_blocks').
+	console.log(index);
+});
