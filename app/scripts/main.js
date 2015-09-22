328209e0280b58869da14758bbc066144bc65c93
Nikita Solomennikov <nikitosgroover@gmail.com>
2015-09-22 16:49:39 +0300

    add item and slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,9 +20,11 @@ jQuery(document).ready(function($) {
   // Array slider parameters
   var ranges = {
     'min': [ 0 ],
-    'max': [ 10000 ]
+    'max': [ 14000 ]
   };
   // Init slider
+  $('.ui-slider-description .min').html(ranges.min);
+  $('.ui-slider-description .max').html(ranges.max);
   noUiSlider.create(ExampleSlider, {
     connect: true,
     behaviour: 'tap',
@@ -46,5 +48,35 @@ jQuery(document).ready(function($) {
 
   inputMax.addEventListener('change', function(){
     ExampleSlider.noUiSlider.set([null, this.value]);
+  });
+  // Owl carousel
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        480:{
+            items:2,
+            nav:false
+        },
+        680:{
+            items:3,
+            nav:false
+        },
+        880:{
+            items:4,
+            nav:true,
+            loop:false
+        },
+        1280:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
   });
 });
